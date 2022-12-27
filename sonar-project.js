8cb734741b1449195b2d699f6caae2430e870638
varun-polampalli <118754117+varun-polampalli@users.noreply.github.com>
2022-12-27 17:41:05 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,7 +1,7 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://43.204.149.82:9000/',
+  serverUrl: 'http://43.204.107.152:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'varun-nodejs',
